d08f91d64decda53424301297942a44ce5caf13f
dmitriy <d.samoilov@newlinetechnologies.net>
2020-04-02 16:32:27 +0300

    added OpenWeatherApiConfig.java

---
+++ weather_api/src/main/java/com/example/weatherapi/OpenWeatherApiConfig.java
@@ -0,0 +1,6 @@
+package com.example.weatherapi;
+
+final class OpenWeatherApiConfig {
+    final static String API_KEY = "c6c587cb81b6523badd4c3ac6ccb9528";
+    final static String API_URL = "http://api.openweathermap.org/data/";
+}
