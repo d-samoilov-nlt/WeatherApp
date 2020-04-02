0645aadf62357c0eff23bfd7f9d112997fc1d65c
dmitriy <d.samoilov@newlinetechnologies.net>
2020-04-02 16:27:16 +0300

    added INoConnectionDialog

---
+++ app/src/main/java/com/example/weatherapp/common/view/INoConnectionDialog.java
@@ -0,0 +1,6 @@
+package com.example.weatherapp.common.view;
+
+public interface INoConnectionDialog {
+    void show();
+    void hide();
+}
---
+++ app/src/main/java/com/example/weatherapp/fabric/NoConnectionDialogFabric.java
@@ -0,0 +1,28 @@
+package com.example.weatherapp.fabric;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+
+import com.example.weatherapp.R;
+import com.example.weatherapp.common.view.INoConnectionDialog;
+import com.example.weatherapp.common.view.NoConnectionSnackBarDialog;
+
+import static com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE;
+
+public class NoConnectionDialogFabric {
+
+    public static INoConnectionDialog get(@NonNull ViewGroup parent){
+        final LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        final View content = inflater.inflate(R.layout.no_connection_snack_bar, parent, false);
+        final NoConnectionSnackBarDialog.ContentViewCallback viewCallback = new NoConnectionSnackBarDialog.ContentViewCallback(content);
+        final NoConnectionSnackBarDialog customSnackBar = new NoConnectionSnackBarDialog(parent, content, viewCallback);
+
+        customSnackBar.getView().setPadding(0, 0, 0, 0);
+        customSnackBar.setDuration(LENGTH_INDEFINITE);
+
+        return customSnackBar;
+    }
+}
