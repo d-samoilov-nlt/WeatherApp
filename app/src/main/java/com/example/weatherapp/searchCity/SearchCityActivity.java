f11f32ac8df013c3402386dae7e4093246cef025
dmitriy <d.samoilov@newlinetechnologies.net>
2020-04-06 19:10:09 +0300

    added SearchCityActivity.java with UI

---
+++ app/src/main/java/com/example/weatherapp/searchCity/SearchCityActivity.java
@@ -0,0 +1,16 @@
+package com.example.weatherapp.searchCity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.weatherapp.R;
+
+public class SearchCityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_city);
+    }
+}
